95c97d04b4559813e681187788da223c380e9224
rashu-pro <rashu.pro@gmail.com>
2022-11-22 11:28:35 +0600

    fetch data from api test

---
+++ pages/posts/first-post.js
@@ -0,0 +1,3 @@
+export default function FirstPost (){
+    return (<h2 className='text-primary'>First post! </h2>)
+}
---
+++ pages/posts/post-2.js
@@ -0,0 +1,3 @@
+export default function Post2(){
+    return <h2>Post 2</h2>;
+}
