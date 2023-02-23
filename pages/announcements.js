89e254402e3d557d67e34a155ca5be2513be7b07
rashu-pro <rashu.pro@gmail.com>
2023-02-23 21:21:16 +0600

    ticker api changed

--- pages/announcements.js
+++ pages/announcements.js
@@ -4,16 +4,23 @@ import getSlug from "./slug";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 export default function Announcements(){
-  const { data, error } = useSWR('https://secure-api.net/api/v1/company-prayer?slug='+getSlug(), fetcher)
+  const { data, error } = useSWR('https://secure-api.net/api/v1/digital-signage-tickers/?slug='+getSlug(), fetcher)
   if(error) return <p className='text-center'> Failed to load... </p>
   if(!data) return <p className='text-center'>loading...</p>
 
     return (
       <>
-        {data.events.map((data, index)=>(
-          <div key={index} className="me-2 marquee-single">
-            <p className="m-0">{data.description} </p>
-          </div>
+        {data.map((data, index)=>(
+          <>
+            {data.title?(
+              <div key={index} className="me-2 marquee-single">
+              <p className="m-0">{data.title} </p>
+              </div>
+              ):(
+                <></>
+            )}
+
+          </>
         ))}
       </>
     )
--- pages/company-info.js
+++ pages/company-info.js
@@ -3,16 +3,17 @@ import getSlug from "./slug";
 import Image from "next/image";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
-export default function CompanyLogo(){
-    const { data, error } = useSWR('https://secure-api.net/api/v1/company-prayer?slug='+getSlug(), fetcher)
-    if(error) return <p className='text-center'> Failed to load... </p>
-    if(!data) return <p className='text-center'>loading...</p>
+export default function CompanyLogo() {
+  const {data, error} = useSWR('https://secure-api.net/api/v1/company-prayer?slug=' + getSlug(), fetcher)
+  if (error) return <p className='text-center'> Failed to load... </p>
+  if (!data) return <p className='text-center'>loading...</p>
 
-    return (
-        <div className="organization-logo logo-holder">
-            <Image src={data.logoURL}
-                   alt={data.name}
-                   fill />
-        </div>
-    )
+  return (
+    <div className="organization-logo logo-holder">
+      <Image src={data.logoURL}
+             alt={data.name}
+             fill/>
+    </div>
+  )
+
 }
